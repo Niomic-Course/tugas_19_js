697ea52ae2e9691807c114dffaffb411d05979a9
Saddam Husein <saddambinhusein2@gmail.com>
2021-07-09 23:37:07 +0800

    Tugas 19 JS Pengumpulan tugas 19 JS ke Niomic

---
+++ tugas19.js
@@ -0,0 +1,21 @@
+console.log('No. 1:')
+function ulangKata1() {
+    const kata1 = 'Semangat...';
+    console.log(kata1.repeat(5));
+}
+
+ulangKata1();
+
+console.log()
+console.log('No. 2:')
+function ulangKata2() {
+    console.log('*'.repeat(5));
+    console.log('*'.repeat(25));
+    console.log('*'.repeat(45));
+    console.log('*'.repeat(65));
+    console.log('*'.repeat(45));
+    console.log('*'.repeat(25));
+    console.log('*'.repeat(5));
+}
+
+ulangKata2();
